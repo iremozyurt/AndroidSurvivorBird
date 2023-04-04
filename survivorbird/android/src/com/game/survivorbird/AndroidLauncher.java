d3e4c286ef5070b5603477a63b75a42837636e24
irem.ozyurt <iremel2211@hotmail.com>
2023-04-04 15:28:27 +0300

    AndroidSurvivorBirdlibGDX
    AndroidSurvivorBird(libGDX)

---
+++ survivorbird/android/src/com/game/survivorbird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.game.survivorbird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.game.survivorbird.survivorbird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new survivorbird(), config);
+	}
+}
